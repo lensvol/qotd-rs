aad5a7c0f5e9693033181f653d58c4cc0a038a13
Kirill Borisov <lensvol@gmail.com>
2015-09-24 11:14:10 +0300

    Print strfile header.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 extern crate rand;
 extern crate clap;
+extern crate byteorder;
 
 use clap::App;
 use rand::Rng;
 
+use std::io::Cursor;
 use std::io::BufReader;
 use std::io::BufRead;
+use std::io::Read;
 use std::io::Write;
 
 use std::fs::File;
@@ -14,8 +17,41 @@ use std::net::UdpSocket;
 use std::sync::Arc;
 use std::thread;
 
+use byteorder::{BigEndian, ReadBytesExt};
 
-fn load_quotes(filename: String) -> Vec<String>{
+
+fn print_strfile_header(handle: File) {
+	let mut header_field = [0u8; 21];
+
+    let mut file = BufReader::new(&handle);
+    file.read(&mut header_field).unwrap();
+	let mut buf = Cursor::new(&header_field[..]);
+
+	let version: u32 = buf.read_u32::<BigEndian>().unwrap();
+	let number_of_strings: u32 = buf.read_u32::<BigEndian>().unwrap();
+	let longest_length = buf.read_u32::<BigEndian>().unwrap();
+	let shortest_length = buf.read_u32::<BigEndian>().unwrap();
+	let flags = buf.read_u32::<BigEndian>().unwrap();
+	let delim = header_field[20];
+
+	println!("Version:\t{}", version);
+	println!("Strings:\t{}", number_of_strings);
+	println!("Longest:\t{}", longest_length);
+	println!("Shortest:\t{}", shortest_length);
+	println!("Delimeter:\t{:?}", delim as char);
+
+	let flag_set = |mask| {
+		if flags & mask == 1 { "yes" } else { "no" }
+	};
+
+	println!("Randomized:\t{}", flag_set(0x1));	
+	println!("Ordered:\t{}", flag_set(0x2));
+	println!("Encrypted:\t{}", flag_set(0x4));	
+	println!("Comments:\t{}\n", flag_set(0x8));
+}
+
+
+fn load_raw_quotes(filename: String) -> Vec<String> {
 	let mut quotes = Vec::new();
 
 	let f = match File::open(filename) {
@@ -78,22 +114,26 @@ fn main() {
 	let bind_addr_str = matches.value_of("ADDR").unwrap_or("127.0.0.1:17").to_string();
 	let quotes_fn = matches.value_of("FILENAME").unwrap().to_string();
 
-	let shared_quotes = Arc::new(load_quotes(quotes_fn));
+	match File::open(quotes_fn.clone() + ".dat") {
+		Ok(file) => print_strfile_header(file),
+		Err(e) => {println!("{:?}", e);}
+	};
+
+	let shared_quotes = Arc::new(load_raw_quotes(quotes_fn));
 
 	let udp_bind_addr = bind_addr_str.clone();
 	let tcp_bind_addr = bind_addr_str.clone();
 	let tcp_quotes = shared_quotes.clone();
 	let udp_quotes = shared_quotes.clone();
 
-	println!("TCP server listening on port {}.", tcp_bind_addr);
-	println!("UDP server listening on port {}.", udp_bind_addr);
+	println!("TCP/UDP server listening on port {}.", bind_addr_str);
 
 	let tcp_listener_handle = thread::spawn(move || {
 		tcp_handler(udp_bind_addr, &tcp_quotes);
 	});
 
 	let udp_listener_handle = thread::spawn(move || {
-		udp_handler(tcp_bind_addr.clone(), &udp_quotes);
+		udp_handler(tcp_bind_addr, &udp_quotes);
 	});
 
 	tcp_listener_handle.join().unwrap();
